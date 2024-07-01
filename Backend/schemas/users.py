97e844bdc8949ce61b551b182ccc2235fbf0b3ce
Your Name <you@example.com>
2024-07-01 17:58:17 -0600

    modificando

--- Backend/schemas/users.py
+++ Backend/schemas/users.py
@@ -1,15 +1,22 @@
+from typing import List, Union
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserBase(BaseModel):
     usuario: str
     password: str
-    estatus: bool= False
+    created_at:datetime
+    estatus: bool
+    Id_persona: int
     
 class UserCreate(UserBase):
     pass
 
+class UserUpdate(UserBase):
+    pass
+    
 class User(UserBase):
     id:int
-    
+
     class Config:
-        orm_mode = True
+        orm_mode =True
